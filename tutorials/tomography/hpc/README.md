8073786b0d41527985e801a53956e66cc0079174
Cedric Lim <cedlim@stanford.edu>
2026-03-22 17:01:58 -0700

    Added to README.md on HPC tutorials. Also some small edits to  with DDP fixes.

--- tutorials/tomography/hpc/README.md
+++ tutorials/tomography/hpc/README.md
@@ -3,4 +3,43 @@
 Included in this folder are two files:
 
 - `run.sh`: Initializes all necessary parameters for `torch` to see all gpus across nodes using `torchrun`.
+- `run_job.sh`: A sample job script for submitting to the Slurm scheduler.
 - `tomography_recon.py`: Contains the full reconstruction script; similar to `Scripts/tomography_02_full.py`
+
+# Compatibility Table
+
+| System | Python Ver. | PyTorch Ver. | Status |
+|--------|-------------|--------------|--------|
+| NERSC Perlmutter | >3.10 | 2.10.0 | ✅ |
+| NERSC Perlmutter | >3.10 | 2.9.0 | ❓ |
+| NERSC Perlmutter | >3.10 | 2.8.0 | ❓ |
+| NERSC Perlmutter | >3.10 | 2.7.0 | ❓ |
+| NERSC Perlmutter | >3.10 | 2.6.0 | ❓ |
+
+# Installing Conda Environments 
+
+Please refer to: https://docs.nersc.gov/development/languages/python/nersc-python/. It is advised to put your environments in `/global/common/software` for the fastest performance.
+
+You will need to clone the quantem repository: `https://github.com/electronmicroscopy/quantem/` and install it in your conda environment using `pip install -e .`.
+
+# Shifter Docker Environment
+
+**3/22/2026 In Progress**
+
+# FAQ
+
+## Compatibility with HPC Systems
+
+These tutorials have been heavily tested using NERSC's Perlmutter system. They should work on other HPC systems with similar configurations, but may require adjustments to the module loading and conda environment paths.
+
+## How to run?
+
+There are two ways to run the script across multiple nodes. On Perlmutter, you can launch an interactive job using `salloc`. The maximum amount of time for an interactive job is 4 hours, and the command for allocating one can be used here:
+
+`salloc -q interactive -C gpu -A m5241 --nodes=4 --ntasks-per-node=1 --gpus-per-node=4 --cpus-per-task=32 --time=04:00:00`
+
+Once the job is allocated, you can just run `sh run.sh` or `batch run.sh`. 
+
+Alternatively, you can submit a job using `sbatch run_job.sh`. Usual parameters can be adjusted within the file.
+
+These files should work on these tutorial files, but will potentially require adjustments when using your own data.
--- tutorials/tomography/hpc/tomography_recon.py
+++ tutorials/tomography/hpc/tomography_recon.py
@@ -10,163 +10,161 @@ from quantem.core.utils.tomography_utils import fourier_binning
 from quantem.core.visualization import show_2d
 import torch
 
-import warnings
-warnings.filterwarnings("ignore", message="Attempting to run cuBLAS, but there was no current CUDA context") # Ignoring CuBLAS errors for now since it fixes it self.
-
 """
 Example script to run the tomography reconstruction on HPC (NERSC).
 
 - Major difference is not needing to specify the device, as it will be automatically set by the DDP framework.
 
-*Cedric Lim, 2/2/26*
+*Cedric Lim, 3/22/26*
 """
 
+if __name__ == "__main__":
 
-# Load Phantom Dataset
-tilt_series = np.load('../../../data/tilt_series_1_deg_tilt_axis.npy')
-tilt_angles = np.load('../../../data/tilt_angles_1_deg_tilt_axis.npy')
+    # Load Phantom Dataset
+    tilt_series = np.load('../../../data/tilt_series_1_deg_tilt_axis.npy')
+    tilt_angles = np.load('../../../data/tilt_angles_1_deg_tilt_axis.npy')
 
-tilt_series = np.array([fourier_binning(img, (100, 100)) for img in tilt_series]) # Cropped down to 100x100 for speed
+    tilt_series = np.array([fourier_binning(img, (100, 100)) for img in tilt_series]) # Cropped down to 100x100 for speed
 
-dset = TomographyINRDataset.from_data(
-    tilt_stack = tilt_series,
-    tilt_angles = tilt_angles,
-)
+    dset = TomographyINRDataset.from_data(
+        tilt_stack = tilt_series,
+        tilt_angles = tilt_angles,
+    )
 
-# Initialize INR Model
-model = HSiren(alpha = 1, winner_initialization = True)
+    # Initialize INR Model
+    model = HSiren(alpha = 1, winner_initialization = True)
 
-# Initialize INR Object
-obj_inr = ObjectINR.from_model(
-    shape = (100, 100, 100),
-    model = model,
-)
+    # Initialize INR Object
+    obj_inr = ObjectINR.from_model(
+        shape = (100, 100, 100),
+        model = model,
+    )
 
-# Define a logger
-logger = LoggerTomography(
-    log_dir = "../../../outputs/tomography/tutorial_02_scripts/",
-    run_prefix = "inr_tomography_warmup_cosineanneal_hpc",
-    run_suffix = "",
-    log_images_every = 2,
-)
+    # Define a logger
+    logger = LoggerTomography(
+        log_dir = "../../../outputs/tomography/tutorial_02_scripts/",
+        run_prefix = "inr_tomography_warmup_cosineanneal_hpc",
+        run_suffix = "",
+        log_images_every = 2,
+    )
 
-# Initialize INR-Based Tomography Object
-tomo_inr = Tomography.from_models(
-    dset = dset,
-    obj_model = obj_inr,
-    logger = logger,
-    verbose = False,
-)
+    # Initialize INR-Based Tomography Object
+    tomo_inr = Tomography.from_models(
+        dset = dset,
+        obj_model = obj_inr,
+        logger = logger,
+        verbose = False,
+    )
 
-# Define optimizer and scheduler parameters - only optimizing the object.
+    # Define optimizer and scheduler parameters - only optimizing the object.
 
-optimizer_params = {
-    "object": OptimizerParams.Adam(
-        lr = 1e-4,
-    ),
-    # "pose": OptimizerParams.Adam(
-    #     lr = 1e-2,
-    # )
-}
-"""
-All available scheduler params are in `core/ml/optimizer_mixin.py`
-
-Scheduler types: 'cyclic', 'plateau', 'exp', 'gamma', 'linear', 'cosine_annealing'
-Keyword arguments follow PyTorch scheduler documentation.
-"""
-
-scheduler_params = {
-    "object": SchedulerParams.Plateau(
-        mode = "min",
-        factor = 0.5,
-        patience = 10,
-        threshold = 1e-3,
-        min_lr = 1e-7,
-    ),
-    # "pose": SchedulerParams.Plateau(
-    #     mode = "min",
-    #     factor = 0.5,
-    #     patience = 10,
-    #     threshold = 1e-3,
-    #     min_lr = 1e-7,
-    # )
-}
-
-"""
-Defining the constraints that we want to apply to the object and dataset. In this case
-adding a total-variational loss, enforcing positivity, and a shrinkage constraint.
-
-For the dataset we can add a 1-D total-variational loss to the shifts and z-shifts.
-However this may not be necessary depending on the dataset.
-"""
-
-obj_constraints = ObjConstraintParams.ObjINRConstraints(
-    positivity = True,
-    sparsity = 1e-6,
-    tv_vol = 1e-4,
-)
-
-## Dataset constraints not necessarily needed.
-
-dataset_constraints = DatasetConstraintParams.BaseTomographyDatasetConstraints(
-    tv_shifts = 1e-6, # 1-D regularizer for the shift optimization
-    tv_zs = 1e-6, # 1-D regularizer for the z-shift optimization.
-)
-
-
-# Warmup Schedule for 10 epochs
-
-num_samples_per_ray = [
-    (0, 20),
-    (1, 20),
-    (2, 40),
-    (3, 40),
-    (4, 60),
-    (4, 60),
-    (6, 80),
-    (7, 80),
-    (8, 100),
-    (9, 100),
-]
-
-tomo_inr.reconstruct(
-    num_iter = 10,
-    batch_size = 256,
-    optimizer_params = optimizer_params,
-    scheduler_params = scheduler_params,
-    obj_constraints = obj_constraints,
-    dset_constraints = dataset_constraints,
-    num_samples_per_ray = num_samples_per_ray,
-    num_workers = 32,
-)
-
-# Initialzie pose optimizer 
-
-optimizer_params = {
-    "pose": {
-        "type": "adam",
-        "lr": 1e-2,
+    optimizer_params = {
+        "object": OptimizerParams.Adam(
+            lr = 1e-4,
+        ),
+        "pose": OptimizerParams.Adam(
+            lr = 1e-2,
+        )
     }
-}
+    """
+    All available scheduler params are in `core/ml/optimizer_mixin.py`
 
-# Define new schedulers for both optimizers using CosineAnnealing
-scheduler_params = {
-    "object": {
-        "type": "cosine_annealing",
-    },
-    "pose": {
-        "type": "cosine_annealing",
+    Scheduler types: 'cyclic', 'plateau', 'exp', 'gamma', 'linear', 'cosine_annealing'
+    Keyword arguments follow PyTorch scheduler documentation.
+    """
+
+    scheduler_params = {
+        "object": SchedulerParams.Plateau(
+            mode = "min",
+            factor = 0.5,
+            patience = 10,
+            threshold = 1e-3,
+            min_lr = 1e-7,
+        ),
+        "pose": SchedulerParams.Plateau(
+            mode = "min",
+            factor = 0.5,
+            patience = 10,
+            threshold = 1e-3,
+            min_lr = 1e-7,
+        )
     }
-}
 
-# Reconstruct
+    """
+    Defining the constraints that we want to apply to the object and dataset. In this case
+    adding a total-variational loss, enforcing positivity, and a shrinkage constraint.
 
-tomo_inr.reconstruct(
-    num_iter = 100,
-    optimizer_params = optimizer_params,
-    scheduler_params = scheduler_params,
-    num_samples_per_ray = 100,
-)
+    For the dataset we can add a 1-D total-variational loss to the shifts and z-shifts.
+    However this may not be necessary depending on the dataset.
+    """
 
-    
-torch.distributed.destroy_process_group()
+    obj_constraints = ObjConstraintParams.ObjINRConstraints(
+        positivity = True,
+        sparsity = 1e-6,
+        tv_vol = 1e-4,
+    )
+
+    ## Dataset constraints not necessarily needed.
+
+    dataset_constraints = DatasetConstraintParams.BaseTomographyDatasetConstraints(
+        tv_shifts = 1e-6, # 1-D regularizer for the shift optimization
+        tv_zs = 1e-6, # 1-D regularizer for the z-shift optimization.
+    )
+
+
+    # Warmup Schedule for 10 epochs
+
+    num_samples_per_ray = [
+        (0, 20),
+        (1, 20),
+        (2, 40),
+        (3, 40),
+        (4, 60),
+        (4, 60),
+        (6, 80),
+        (7, 80),
+        (8, 100),
+        (9, 100),
+    ]
+
+    tomo_inr.reconstruct(
+        num_iter = 10,
+        batch_size = 256,
+        optimizer_params = optimizer_params,
+        scheduler_params = scheduler_params,
+        obj_constraints = obj_constraints,
+        dset_constraints = dataset_constraints,
+        num_samples_per_ray = num_samples_per_ray,
+        num_workers = 32,
+    )
+
+    # Initialzie pose optimizer 
+
+    optimizer_params = {
+        "pose": {
+            "type": "adam",
+            "lr": 1e-2,
+        }
+    }
+
+    # Define new schedulers for both optimizers using CosineAnnealing
+    scheduler_params = {
+        "object": {
+            "type": "cosine_annealing",
+        },
+        "pose": {
+            "type": "cosine_annealing",
+        }
+    }
+
+    # Reconstruct
+
+    tomo_inr.reconstruct(
+        num_iter = 100,
+        optimizer_params = optimizer_params,
+        scheduler_params = scheduler_params,
+        num_samples_per_ray = 100,
+    )
+
+        
+    torch.distributed.destroy_process_group()
