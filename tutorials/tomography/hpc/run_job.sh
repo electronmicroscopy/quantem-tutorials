fc4727d096b61fd37c925ceb0e668ea730189682
Cedric Lim <cedlim@stanford.edu>
2026-03-23 20:59:38 -0700

    run_job.sh and run.sh, more explicit which ones to change

--- tutorials/tomography/hpc/run_job.sh
+++ tutorials/tomography/hpc/run_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A mxxxx
+#SBATCH -A mxxxx  # CHANGE: Replace with your NERSC project ID
 #SBATCH -C gpu
 #SBATCH -q debug
 #SBATCH -t 00:30:00
@@ -9,7 +9,7 @@
 #SBATCH --gpus-per-node=4
 #SBATCH --gpu-bind=none
 #SBATCH --mail-type=begin,end,fail
-#SBATCH --mail-user=xxx@xxx.xxx
+#SBATCH --mail-user=xxx@xxx.xxx  # CHANGE: Replace with your email address
 
 export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 export MASTER_PORT=29500
@@ -17,5 +17,5 @@ export OMP_NUM_THREADS=8
 ml nccl/2.29.2-cu13
 ml conda
 ml nccl
-conda activate /global/common/software/mxxxx/'INSERT USER HERE'/conda/quantem
+conda activate /global/common/software/mxxxx/'INSERT USER HERE'/conda/quantem  # CHANGE: Replace 'INSERT USER HERE' with your NERSC username and update path to your conda environment
 srun -l torchrun --nnodes=$SLURM_JOB_NUM_NODES --nproc-per-node=$SLURM_GPUS_PER_NODE --rdzv-backend=c10d --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT tomography_recon.py
